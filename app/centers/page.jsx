cc36a74d740c729c40551466e4cc676059b1e454
Abhiverma21 <abhishekjbverma@gmail.com>
2025-06-26 15:16:27 +0530

    updates by abhi

---
+++ app/centers/page.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Layout from "@/components/layout/Layout.js"
+import Center from "@/components/Center/Center.jsx"
+
+
+export default function page() {
+    return (
+        <>
+            <Layout headerStyle={3} footerStyle={4} >
+                <div className="main">
+                    <Center />
+                </div>
+
+
+            </Layout>
+
+
+
+        </>
+    )
+
+};
