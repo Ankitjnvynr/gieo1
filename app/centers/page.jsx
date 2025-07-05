031ff14420cd9bde4a442f9d01c777d7d26cb969
ankitjnvynr <ankitbkana@outlook.com>
2025-07-05 21:14:15 +0530

    Enhance layout of page component by adjusting styles and adding title for better visibility

--- app/centers/page.jsx
+++ app/centers/page.jsx
@@ -13,14 +13,21 @@ export default function page() {
         width: "100%",
         display: "flex",
         alignItems: "center",
+        backgroundColor:'#a59069',
+        justifyContent:'center'
       }}>
         <img style={{
           width: "50%",
           margin: "0 auto",
           height: "300px",
           objectFit: "cover",
-          backgroundPosition: 'top'
+          mixBlendMode: "multiply",
         }} src="/assets/images/Center/mainImg.jpg" alt="" />
+
+        <h2
+        style={{position:"absolute",color:'white',}}
+        >Gita Gyan Sansthanam - All Centers</h2>
+
       </div>
 
       <Info />
