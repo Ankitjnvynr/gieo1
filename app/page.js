09aae4b8760767f61a568f7d32d547c12f4a13a9
ankitjnvynr <ankitbkana@outlook.com>
2025-07-08 15:14:56 +0530

    fix: comment out unused Banner2 component in Home page

--- app/page.js
+++ app/page.js
@@ -26,7 +26,7 @@ export default function Home() {
                 <ServicesFive />
                 <AboutFour />
                 <BrandTwo />
-                <Banner2/>
+                {/* <Banner2/> */}
                 <ProjectFour />
                 <SlidingText />
                 <AboutThree/>
