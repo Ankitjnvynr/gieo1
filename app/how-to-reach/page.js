23ad3d978806fdb33d35a940aa10c33ce6d55b7b
ankitjnvynr <ankitbkana@outlook.com>
2025-07-09 17:18:32 +0530

    fix: correct import path for HeroSection component in how-to-reach page

--- app/how-to-reach/page.js
+++ app/how-to-reach/page.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Layout from "@/components/layout/Layout"
-import HeroSection from '@/components/howtoreach/heroSection'
 import AboutSection from '@/components/howtoreach/AboutSection'
 import TransportModes from '@/components/howtoreach/TransportModes'
 import Maps from '@/components/howtoreach/Maps'
 import TravelTips from '@/components/howtoreach/TravelTips'
 import '@/components/howtoreach/page1.css'
+import HeroSection from '@/components/howtoreach/HeroSection'
 
 const page = () => {
   return (
