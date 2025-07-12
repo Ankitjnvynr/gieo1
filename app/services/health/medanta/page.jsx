8b11559a7d3f8a0c084f5963566ec0dbb86444c2
ankitjnvynr <ankitbkana@outlook.com>
2025-07-12 10:42:43 +0530

    fix: remove extra line in Home component for cleaner code

--- app/services/health/medanta/page.jsx
+++ app/services/health/medanta/page.jsx
@@ -29,6 +29,7 @@ export default function Home() {
                 <WhyWe />
                 <VideoOne />
                 <WorkProcess />
+                
                 <BlogThree />
             </Layout>
         </>
